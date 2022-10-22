ecd883c169a0830d46dee8761a0d339576c42521
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-22 23:18:19 +0900

    Update configStore.js

--- redux/configStore.js
+++ redux/configStore.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import bucket from "./modules/bucket";
 
+const middlewares = [thunk];
 const rootReducer = combineReducers({ bucket });
+const enhancer = applyMiddleware(...middlewares);
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
